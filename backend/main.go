81c0c060fa753a1b478522e4496ee8e99a1fca83
ubombar <ufukbombar@gmail.com>
2024-01-16 15:24:04 +0100

    Add sqlite support in local runs

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 	"time"
@@ -9,7 +8,9 @@ import (
 	"github.com/RootLeo00/book-web-app-daar/pkg/backend"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 const MAX_TRIES = 3
@@ -21,26 +22,35 @@ func main() {
 	var err error
 
 	if dsn, ok = os.LookupEnv("DATABASE_URL"); !ok {
-		panic("environment variable DATABASE_URL is not provided")
-	}
+		logger.Default.Info(nil, "environment variable DATABASE_URL is not provided, looking for the local sql file...")
 
-	for tries := 1; tries <= MAX_TRIES; tries++ {
-		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		db, err = gorm.Open(sqlite.Open("./db.sqlite3"), &gorm.Config{})
 
 		if err != nil {
-			if tries < MAX_TRIES {
-				// Sleep for 1 second to try again
-				fmt.Println("cannot connect to the database")
-				time.Sleep(time.Second)
-			} else {
-				panic("cannot connect to the database, all tries failed")
-			}
-		} else {
-			break
+			panic("cannot open to the sqlite3 database")
 		}
+
+	} else {
+		// Try to connect to the postgres, if cannot connect sleep for 1 second and try again
+		for tries := 1; tries <= MAX_TRIES; tries++ {
+			db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+			if err != nil {
+				if tries < MAX_TRIES {
+					// Sleep for 1 second to try again
+					logger.Default.Info(nil, "cannot connect to the database")
+					time.Sleep(time.Second)
+				} else {
+					panic("cannot connect to the database, all tries failed")
+				}
+			} else {
+				break
+			}
+		}
+
 	}
 
-	fmt.Println("Connected to the database, migrating...")
+	logger.Default.Info(nil, "Connected to the database, migrating...")
 
 	// Automatically migrate the following models
 	db.AutoMigrate(
@@ -65,7 +75,7 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	fmt.Println("Starting server at port 8080...")
+	logger.Default.Info(nil, "Starting server at port 8080...")
 	if err := server.ListenAndServe(); err != nil {
 		panic(err)
 	}
