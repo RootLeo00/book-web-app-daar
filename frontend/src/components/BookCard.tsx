37f2f392e15125cc4bc888693492ebf30cfd3e68
RootLeo00 <caterina.leonelli@gmail.com>
2024-01-22 17:23:01 +0100

    card component to display book details

---
+++ frontend/src/components/BookCard.tsx
@@ -0,0 +1,38 @@
+import { CardMedia, Card, CardHeader, Avatar, CardContent, Typography } from '@mui/material';
+import red from '@mui/material/colors/red';
+import avatar from '../images/avatar_icon.png';
+
+
+const BookCard = ({ cardData }: any) => {
+
+    return ((cardData !== null || cardData !== undefined ) ? (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+                title={cardData?.title}
+                subheader={(cardData?.author === undefined || cardData?.author === "None") ? ("No Author") : (cardData?.author)} //TODO to fix python backend
+                avatar={
+                    <Avatar
+                        src={avatar}
+                        sx={{ bgcolor: red[500], width: 56, height: 56 }}
+                    />
+                }
+            />
+            <CardMedia
+                component="img"
+                height="500"
+                image={cardData?.imageUrl}
+                alt="Loading card..."
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    {cardData?.text}
+                </Typography>
+                <Typography variant="caption" color="text.secondary">
+                    Crank: {cardData?.crank}
+                </Typography>
+            </CardContent>
+        </Card>
+    ) : (<p>loading card</p>))
+}
+
+export default BookCard;
